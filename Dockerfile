ab446636e8ac2312b87d66f99f85ceb345b3e813
Jason Fleming <jfleming9357@gmail.com>
2021-02-03 12:19:11 -0500

    fixes to docker

--- Dockerfile
+++ Dockerfile
@@ -7,6 +7,6 @@ COPY . .
 RUN npm install
 
 EXPOSE 8080
-EXPOSE 27018
 EXPOSE 8081
+EXPOSE 27018
 CMD [ "node", "server.js" ]
--- config/db.js
+++ config/db.js
@@ -2,7 +2,7 @@ const mongoose = require('mongoose');
 
 const connectDB = async (db) => {
   try {
-    const conn = await mongoose.connect(`mongodb://ec2-18-217-126-196.us-east-2.compute.amazonaws.com /sdc_merged`, {
+    const conn = await mongoose.connect(`mongodb://localhost:27018/sdc_merged`, {
       useNewUrlParser: true,
       useCreateIndex: true,
       useFindAndModify: true,
